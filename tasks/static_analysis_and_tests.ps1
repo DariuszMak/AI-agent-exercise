a783089ffbee1ecff7bcd4f50429b27413061882
Dariusz Makarewicz <darekmakarewicz1994@gmail.com>
2026-06-01 15:11:04 +0200

    split slow and not slow tests

--- tasks/static_analysis_and_tests.ps1
+++ tasks/static_analysis_and_tests.ps1
@@ -1,7 +1,8 @@
 .\scripts\format_and_lint.ps1 ; 
 
-uv run pytest tests/ --cov=src --cov-report=html --cov-report=xml --cov-config=.coveragerc -vv -m "slow" ; 
+uv run pytest tests/ --cov=src --cov-report=html --cov-report=xml --cov-config=.coveragerc -vv -m "not slow" ; 
 
 uv run deepeval test run tests/eval/test_rag_accuracy.py ; 
+uv run pytest tests/ --cov=src --cov-report=html --cov-report=xml --cov-config=.coveragerc -vv -m "slow" ; 
 
 Start-Process .\htmlcov\index.html ; 
