2d014e30d32ea000ac3d7310acfa6436557a93ab
Dariusz Makarewicz <darekmakarewicz1994@gmail.com>
2026-06-01 15:00:15 +0200

    Revert "disable mcp client server"
    This reverts commit 9814d7c77724bfe923a928812e6be0d88945a012.

--- tasks/run_windows_dev_application.ps1
+++ tasks/run_windows_dev_application.ps1
@@ -1,3 +1,3 @@
-# Start-Process uv -ArgumentList "run", "python", "-m", "src.mcp_client.server" ; 
+Start-Process uv -ArgumentList "run", "python", "-m", "src.mcp_client.server" ; 
 Start-Process uv -ArgumentList "run", "python", "src\agent_main.py" ; 
 Start-Process uv -ArgumentList "run", "python", "src\main.py" ; 
