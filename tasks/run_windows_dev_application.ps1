0506413b44429d3dd7009ab79922b96e449278e0
Dariusz Makarewicz <darekmakarewicz1994@gmail.com>
2026-06-02 18:03:09 +0200

    update api server path

--- tasks/run_windows_dev_application.ps1
+++ tasks/run_windows_dev_application.ps1
@@ -1,3 +1,3 @@
 Start-Process uv -ArgumentList "run", "python", "-m", "src.mcp_client.mcp_server" ; 
 Start-Process uv -ArgumentList "run", "python", "src\agent_runner.py" ; 
-Start-Process uv -ArgumentList "run", "python", "src\server.py" ; 
+Start-Process uv -ArgumentList "run", "python", "src\rag\api\server.py" ; 
