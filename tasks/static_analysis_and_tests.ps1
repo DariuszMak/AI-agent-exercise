f053bb4ad073cfa8eec20c430770e73baab81af3
Dariusz Makarewicz <darekmakarewicz1994@gmail.com>
2026-06-10 19:43:17 +0200

    another approach to test

--- tasks/static_analysis_and_tests.ps1
+++ tasks/static_analysis_and_tests.ps1
@@ -3,6 +3,6 @@
 uv run pytest tests/ --cov=src --cov-report=html --cov-report=xml --cov-config=.coveragerc -vv -m "not slow" ; 
 uv run pytest tests/ --cov=src --cov-report=html --cov-report=xml --cov-config=.coveragerc -vv -m "slow" ; 
 
-uv run deepeval test run tests/eval/test_rag_accuracy.py ; 
+uv run pytest tests/eval/test_rag_accuracy.py ;  
 
 Start-Process .\htmlcov\index.html ; 
