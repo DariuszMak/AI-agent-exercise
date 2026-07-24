5ee03539f7892c9cd6aa0fabbc4b42b42bd205dd
Dariusz <darekmakarewicz1994@gmail.com>
2026-07-24 11:06:36 +0200

    add changes

--- tasks/run_native_dev_application.ps1
+++ tasks/run_native_dev_application.ps1
@@ -1,3 +1,4 @@
+New-Item -ItemType Directory -Force -Path .\logs | Out-Null
 Start-Process uv -ArgumentList "run", "python", "-m", "src.mcp_client.mcp_server" ; 
 Start-Process uv -ArgumentList "run", "python", "src\agent_runner.py" ; 
 Start-Process uv -ArgumentList "run", "python", "src\rag\api\server.py" ; 
